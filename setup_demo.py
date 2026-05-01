0b8377994dbe1645ce5d432dbaa4b168c089bf37
marquisedemaree <marquise.demaree@gmail.com>
2026-04-30 22:03:22 -0700

    Update setup_demo.py

--- setup_demo.py
+++ setup_demo.py
@@ -1,6 +1,10 @@
+import ssl
+import certifi
 import urllib.request
 
-urllib.request.urlretrieve(
-    "https://github.com/marquisedemaree/RAT/releases/download/v1.0/data.zip",
-    "models.zip"
-)
+url = "https://github.com/marquisedemaree/RAT/releases/download/v1.0/data.zip"
+out = "https://github.com/marquisedemaree/RAT/releases/download/v1.0/models.zip"
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+urllib.request.urlretrieve(url, out)
